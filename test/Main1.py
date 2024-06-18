758e722d7dd3dfd82c9f562293cc9598e95455b4
Vaibhav Sinha <vaibhavrajsinha099@gmail.com>
2024-06-18 21:28:26 +0530

    second comment

---
+++ test/Main1.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import talib
+import math
+import requests
+import http
+from pybit.unified_trading import HTTP
+import hmac
+import hashlib
+import time
+import urllib.parse
+import base64
+import Creds
+
+class StrategyName:
+    def FetchData(self):
+        pass
+
+    def Functions(self):
+        pass
+
+    def Signal(self):
+        pass
+
+    def ExecuteTrades(self):
+        pass
+
+    def LogicPL(self):
+        pass
+
+    def Plot(self):
+        pass
+
+
+class RiskManagement:
+    pass
+
+def main():
+    pass
+
+main()
